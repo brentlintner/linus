b443770af41b62fc72ac076e5feaab936f8ba897
Brent Lintner <brent.lintner@gmail.com>
2025-03-15 16:47:44 -0400

    Tweaking the theme some more.

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ GEMINI_MODEL = os.getenv('GEMINI_MODEL') or ''
 PROMPT_PREFIX_FILE = os.path.join(os.path.dirname(__file__), '../docs/background.md')
 DEFAULT_IGNORE_PATTERNS = ['.git*']
 CONSOLE_THEME = Theme({
-    "markdown.code": "#dcdcdc",
+    "markdown.code": "#a7c080",
     "markdown.code_block": "#dcdcdc",
     "markdown.h1": "bold #dcdcdc",
     "markdown.h2": "bold #dcdcdc",
@@ -17,6 +17,10 @@ CONSOLE_THEME = Theme({
     "markdown.strong": "bold #dcdcdc",
     "markdown.em": "#a7c080",
     "markdown.alert": "bold #e67e80",
+    "markdown.paragraph": "#dcdcdc",
+    "markdown.item.bullet": "bold #dbbc7f",
+    "markdown.item.number": "bold #dbbc7f",
+    "markdown.item.text": "#dcdcdc",
     "diff.add": "#a7c080",
     "diff.remove": "#e67e80",
     "diff.header": "#dbbc7f"
