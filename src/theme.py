d36b58aa5bdcbb0dcd7b1469c175fbdbffc749aa
Brent Lintner <brent.lintner@gmail.com>
2025-03-15 16:39:04 -0400

    Tweak theme more

--- src/theme.py
+++ src/theme.py
@@ -15,38 +15,52 @@ from pygments.token import (
 class EverforestDarkStyle(Style):
     name = "everforest-dark"
 
-    background_color = None # OR: #0f0f0f
+    # Color Palette
+    # background = 000100
+    # foreground = dcdcdc
+    # black = 4b565c
+    # blue = 7fbbb3
+    # cyan = 83c092
+    # green = a7c080
+    # magenta = d699b6
+    # red = e67e80
+    # white = d3c6aa
+    # yellow = dbbc7f
+    # orange = 83C092
+
     line_number_color = "#627e79"
+    background_color = None
 
     styles = {
         Comment: "noitalic #859289",
         Comment.Preproc: "noitalic #7fbbb3",
         Comment.Special: "noitalic bg:#ffffff",
-        Keyword: "bold #e67e80",
-        Keyword.Pseudo: "nobold #dcdcdc",
+        Keyword: "nobold #e67e80",
+        Keyword.Pseudo: "nobold #d699b6",
         Keyword.Type: "nobold #e67e80",
+        Keyword.Constant: "nobold #d699b6",
         Operator: "#e69875",
-        Operator.Word: "bold #e67e80",
+        Operator.Word: "#83C092",
         Name: "#d3c6aa",
-        Name.Builtin: "#d699b6",
+        Name.Builtin: "#a7c080",
         Name.Function: "#a7c080",
         Name.Class: "#e67e80",
-        Name.Namespace: "#e67e80",
+        Name.Namespace: "#dbbc7f",
         Name.Exception: "#e67e80",
         Name.Variable: "#d699b6",
         Name.Constant: "#d699b6",
-        Name.Label: "bold #d3c6aa",
-        Name.Entity: "bold #e67e80",
+        Name.Label: "#d3c6aa",
+        Name.Entity: "#e67e80",
         Name.Attribute: "#7fbbb3",
-        Name.Tag: "bold #a7c080",
-        Name.Decorator: "bold #e69875",
+        Name.Tag: "#a7c080",
+        Name.Decorator: "bold #e67e80",
         String: "#83C092",
         String.Doc: "noitalic",
         String.Interpol: "noitalic #d3c6aa",
-        String.Escape: "bold #83C092",
+        String.Escape: "#83C092",
         String.Regex: "#83C092",
         String.Symbol: "#d699b6",
-        String.Other: "#e69875",
+        String.Other: "#e67e80",
         Number: "#d699b6",
         Punctuation: "#d3c6aa",
         Generic.Heading: "bold #d3c6aa",
@@ -57,7 +71,7 @@ class EverforestDarkStyle(Style):
         Generic.Emph: "noitalic #dcdcdc",
         Generic.Strong: "bold #dcdcdc",
         Generic.EmphStrong: "bold #dcdcdc",
-        Generic.Prompt: "bold #e69875",
+        Generic.Prompt: "bold #e67e80",
         Generic.Output: "#d3c6aa",
         Generic.Traceback: "#e67e80",
         Error: "border:#e67e80",
